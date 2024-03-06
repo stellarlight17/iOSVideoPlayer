9d66860a9fdc098cfcfbd15c3f56f8a6d8e15727
Stella <stellacheung1919@gmail.com>
2024-03-06 18:03:01 +0800

    update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .target(
             name: "VideoPlayer", dependencies: []),
         //.binaryTarget(name: "HorizontalProgressBarView", path: "../HorizontalProgressBarView/HorizontalProgressBarView.xcframework"),
-        .binaryTarget(name: "HorizontalProgressBarView", url: "https://github.com/stellarlight17/HorizontalProgressBarView/releases/download/0.1.0/HorizontalProgressBarView.xcframework.zip", checksum: "55b9b1293d26b8d9acc60210fd28dc67e1bf1fa8a8d98b5f037edc5db48c831d"),
+        .binaryTarget(name: "HorizontalProgressBarView", url: "https://github.com/stellarlight17/HorizontalProgressBarView/releases/download/1.0.0/HorizontalProgressBarView.xcframework.zip", checksum: "55b9b1293d26b8d9acc60210fd28dc67e1bf1fa8a8d98b5f037edc5db48c831d"),
         .testTarget(
             name: "VideoPlayerTests",
             dependencies: ["VideoPlayer"]),
